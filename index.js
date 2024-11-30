f6e5930fdcaef7b4af612f79c625a3ea4c04b39f
Keshav jha <keshavjhaa2678@gmail.com>
2024-11-30 06:52:48 +0000

    added cors

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const cors = require("cors");
+app.use(cors());
 // Middleware
 app.use(bodyParser.json());
 
